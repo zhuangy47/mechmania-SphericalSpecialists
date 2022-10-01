4ed46f5ddd28575c1399db67058d0aa10708b508
zhuangy47 <35969859+zhuangy47@users.noreply.github.com>
2022-09-30 23:59:41 -0500

    commit message

---
+++ strategy/main_strat.py
@@ -0,0 +1,24 @@
+from random import Random
+from game.game_state import GameState
+import game.character_class
+
+from game.item import Item
+
+from game.position import Position
+from strategy.strategy import Strategy
+
+class MainStrategy(Strategy):
+    def strategy_initialize(self, my_player_index: int):
+        return game.character_class.CharacterClass.WIZARD
+
+    def move_action_decision(self, game_state: GameState, my_player_index: int) -> Position:
+        return game_state.player_state_list[my_player_index].position
+
+    def attack_action_decision(self, game_state: GameState, my_player_index: int) -> int:
+        return Random().randint(0, 3)
+
+    def buy_action_decision(self, game_state: GameState, my_player_index: int) -> Item:
+        return Item.NONE
+
+    def use_action_decision(self, game_state: GameState, my_player_index: int) -> bool:
+        return False
